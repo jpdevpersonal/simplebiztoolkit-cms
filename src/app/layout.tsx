fc38538708915245637b326b33e151a66d4949e4
julian pickard <jpickard.mail@gmail.com>
2026-02-15 17:38:49 +0000

    Remove analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,13 +105,6 @@ export default function RootLayout({
           src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
           strategy="afterInteractive"
         />
-        <Script id="ga-init" strategy="afterInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', '${gaId}');`}
-        </Script>
       </head>
       <body>
         <AuthProvider>
